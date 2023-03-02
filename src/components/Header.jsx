85e1d15a663a72c5be31d8aa3cf16350a7b478d9
Manomay Kagalkar <manomay111@gmail.com>
2023-03-02 11:20:24 +0530

    header progress
    + made sticky and minimising
    
    pending
    - header components must minimise
    - dropdown nav menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
 import './Header.css'
 import logo from '../resources/TEDXVITB-logo-white.png'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
+
+    // hooks
+    let [scrollPostion, setScrollPosition] = useState(0)
+    useEffect(() => {
+        document.addEventListener('scroll', onScroll, { passive: true })
+        return () => {
+            document.removeEventListener('scroll', onScroll)
+        }
+    })
+
+    // handlers
+    const onScroll = () => {
+        setScrollPosition(window.scrollY)
+    }
+
     return(
-        <header className='header'>
+        <header className={scrollPostion>0? 'header mini':'header'}>
             <img src={logo} alt="tedxvitbhopal logo" className='header-logo'/>
             <nav className='header-navbar'>
                 <a href="/" className='navlink'>Home</a>
@@ -11,6 +27,13 @@ const Header = () => {
                 <a href="/" className='navlink'>About</a>
                 <a href="/" className='navlink'>Attend</a>
                 <a href="/" className='navlink'>Watch</a>
+            </nav>
+            <nav className='header-navbar-dropdown'>
+                <a href="/" className='navItem'>Home</a>
+                <a href="/" className='navItem'>Discover</a>
+                <a href="/" className='navItem'>About</a>
+                <a href="/" className='navItem'>Attend</a>
+                <a href="/" className='navItem'>Watch</a>
             </nav>
         </header>
     )
