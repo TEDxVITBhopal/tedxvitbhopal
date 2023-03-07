704f5f57b6735eb2f75b75b0a6ccb6ebe74c4c1b
Kshitij Singh <66397721+notkshitijsingh@users.noreply.github.com>
2023-03-07 21:58:55 +0530

    Update OurMessage.jsx

--- src/components/home/OurMessage.jsx
+++ src/components/home/OurMessage.jsx
@@ -20,13 +20,7 @@ function OurMessage() {
             the spirit of TED to local communities around the globe through TEDx
             events. These events are organized by passionate individuals who seek to
             uncover new ideas and to share the latest research in their local areas
-            that spark conversations in their communities. TEDx events include live
-            speakers and recorded TED Talks, and are organized independently under a
-            free license granted by TED. These events are not controlled by TED, but
-            event organizers agree to abide by our format, and are offered
-            guidelines for curation, speaker coaching, event organizing and more.
-            They learn from us and from each other. More than 3000 events are now
-            held annually.
+            that spark conversations in their communities.
           </div>
         </div>
       </div>
