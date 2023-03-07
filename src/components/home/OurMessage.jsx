50cb6dffe57d88f2d2fe4cdae576c1e3f3304c09
Kshitij Singh <66397721+notkshitijsingh@users.noreply.github.com>
2023-03-08 03:36:14 +0530

    Cosmic Butterfly

--- src/components/home/OurMessage.jsx
+++ src/components/home/OurMessage.jsx
@@ -12,15 +12,10 @@ function OurMessage() {
         <div className="right-side">
           <div className="center">
             <span className="red">Cosmic&nbsp;</span>
-            <span className="black">Butterflies</span>
+            <span className="black">Butterfly</span>
         </div>
           <div className="content1">
-            TEDx is a grassroots initiative, created in the spirit of TED’s overall
-            mission to research and discover “ideas worth spreading.” TEDx brings
-            the spirit of TED to local communities around the globe through TEDx
-            events. These events are organized by passionate individuals who seek to
-            uncover new ideas and to share the latest research in their local areas
-            that spark conversations in their communities.
+          TEDx VIT Bhopal's chapter theme of "Cosmic Butterfly" is in itself a mission to explore the intersection of science, art, and the metaphysical, and to promote a deeper understanding of the interconnectedness of all things in the universe. We hope to evoke awe and curiosity about the mysteries of the cosmos and encourage attendees to think deeply about how we can create a more sustainable and harmonious relationship with the world naturally, and as such - the message of TEDx VIT Bhopal's "Cosmic Butterfly" theme is to ignite action and build together a diverse community of individuals who are passionate about creating positive change and fostering a greater appreciation for the beauty and complexity of the universe. Speakers at the event explore a wide range of topics, from the latest scientific discoveries about the universe to the metaphysical and physiological perspectives on our place in it. Ultimately, the mission of TEDx VIT Bhopal's "Cosmic Butterfly" theme is to foster a sense of wonder, curiosity, and connection among attendees, and to inspire them to take action towards creating a more sustainable and harmonious world.
           </div>
         </div>
       </div>
