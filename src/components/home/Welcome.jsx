8d97240bda904d7d7a51eff3d0d193b0d1eea1eb
Manomay Kagalkar <manomay111@gmail.com>
2023-03-09 12:53:04 +0530

    welcome screen responsiveness

--- src/components/home/Welcome.jsx
+++ src/components/home/Welcome.jsx
@@ -4,17 +4,17 @@ import WelcomeImg from "../../resources/tempo-welcome.png";
 
 const WelcomeScreen = () => {
     return(
-        <>
-        <div className="left">
-            <img src={WelcomeImg} alt={"Welcome"} className="welcome-img"/>
-        </div>
-        <div className="right">
-            <h1 className="quote">
+        <section className="welcome-section">
+            <figure className="welcome-pane">
+                <img src={WelcomeImg} alt="Welcome" className="welcome-image"/>
+            </figure>
+            <blockquote className="welcome-pane">
+                <h1 className="quote">
                 Vision begins with the eyes,
                 <span className="red"> but it truly takes place in the brain</span>.
-            </h1>
-        </div>
-        </>
+                </h1>
+            </blockquote>
+        </section>
     )
 }
 
