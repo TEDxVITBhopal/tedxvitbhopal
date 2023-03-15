264233ef8c235e7d7fcb3bf216fc11782d567d46
Kshitij Singh <66397721+notkshitijsingh@users.noreply.github.com>
2023-03-15 14:31:14 +0530

    BlogPage started

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import Header from "../components/Header";
 import Event from "./Event";
 import Footer from '../components/Footer'
+import BlogPage from "../components/blogs/blogPage";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             </>
           }
         />
+        <Route path="/blog" element={<BlogPage/>} />
         <Route path="/*" element={<NotFound />} />
         <Route path="/event" element={<Event />} />
       </Routes>
