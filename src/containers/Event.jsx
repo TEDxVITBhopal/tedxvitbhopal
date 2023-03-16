2a54c7a26f922f6eeba5580574852eeca6d55991
Manomay Kagalkar <manomay111@gmail.com>
2023-03-16 22:23:30 +0530

    started slideshow work

--- src/containers/Event.jsx
+++ src/containers/Event.jsx
@@ -6,13 +6,24 @@ import './Event.css'
 import speakerImg from '../resources/test-person.jpg';
 // venue image (yes we change this)
 import VITImage from "../resources/vit.jpg";
+import bullseye from '../resources/bullseye.png'
+import ellipse from '../resources/ellipse.png'
 
 const Event = () => {
   return (
     <>
-      <div className='page' id='slideshow'>
-        slideshow
-      </div>
+      <section className='slideshow-page'>
+        <menu className="slideshow-menu">
+          <img src={bullseye} alt="" />
+          <div className='slideshow-bullseye'>
+            <div className='slideshow-ring'>
+              <div className='slideshow-dot'></div>
+              <div className='slideshow-dot two'></div>
+            </div>
+          </div>
+        </menu>
+        <section className='slideshow-images'>slides</section>
+      </section>
       <div className='page' id='venue'>
         <img id="venue-photo" src={VITImage} alt={"VITBhopal"}/>
         <h1 id="college-name">Vellore Institute of Technology, Sehore</h1>
