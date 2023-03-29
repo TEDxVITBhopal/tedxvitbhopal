6b60dd240bb9e86279db1f0664fe1336d9f91875
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-03-30 00:49:54 +0530

    team page completed

--- src/components/team/SingleMember.js
+++ src/components/team/SingleMember.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import styles from "./Heads.module.css" ; 
+import React from "react";
+import styles from "./Heads.module.css";
 function SingleMember(props) {
   return (
-<div classname={styles.flex}>
-    <div className={styles.flexs}>
-      <img className={styles.pic} src= {props.pic} />
-       <div className= {styles.flexs}>
-      <div><b>{props.pos}</b></div>
-      <div>{props.name}</div>
-  </div>
-</div>
-</div>
-  )
+    <div classname={styles.flex}>
+      <div className={styles.flexs}>
+        <img className={styles.pic} src={props.pic} alt="img" />
+        <div className={styles.flexs}>
+          <div>
+            <b>{props.pos}</b>
+          </div>
+          <div>{props.name}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SingleMember
+export default SingleMember;
