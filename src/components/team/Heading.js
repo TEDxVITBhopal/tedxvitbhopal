308a57011dd6820537125605bc4cc7328d030074
Kshitij Singh <66397721+notkshitijsingh@users.noreply.github.com>
2023-04-04 14:51:37 +0530

    added content

--- src/components/team/Heading.js
+++ src/components/team/Heading.js
@@ -8,13 +8,7 @@ const Heading = () => {
       </div>
       <div className={styles.text}>
         <span>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Our teams are composed of highly skilled individuals who are passionate about their work and strive to produce exceptional results. Each team brings a unique set of skills and expertise, collaborating seamlessly to achieve our shared goals. From Content & Editorial to Web & App and Registrations, our teams work cohesively and collaborate to deliver top-notch products and services. With a diverse range of talents and backgrounds, we bring a wealth of perspectives to the table, fostering a culture of innovation and creativity. We are dedicated to delivering excellence, whether it's creating compelling content, driving our ideas to fruition, or enhancing community experience. Together, we are a dynamic and successful team, working to make the TEDx dream experience a reality.
         </span>
       </div>
     </div>
