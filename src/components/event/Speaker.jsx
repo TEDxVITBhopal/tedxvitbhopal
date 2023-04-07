cd1a088f7fbc48e8154f68846ff804611e797425
smarani19 <titlibas.123@gmail.com>
2023-04-07 21:36:04 +0530

    speaker section

--- src/components/event/Speaker.jsx
+++ src/components/event/Speaker.jsx
@@ -1,51 +1,105 @@
 import React from 'react';
 import globe from '../../resources/globe.png';
-import './Speaker.css';
+import image from "./resources/PUSHPDANT_JAIN.png"; 
+import Speaker from './Speaker.css';
 import mic from '../../resources/mic.png'
 
-function Speaker({images}) {
 
-  let showBottom = true;
+  // let showBottom = true;
 
-  if (images.length <= 4){
-     showBottom = false;
-  }
-  return (
-    <div className="container">
-      <span className='heading1'>The Speakers</span>
-      <div className="flexImages">
-        <div className="imageWrapper">
-          <img className='globe' src={globe} alt="" />
+  // if (images.length <= 4){
+  //    showBottom = false;
+  // }
+  // return (
+  //   <div className="container">
+  //     <span className='heading1'>The Speakers</span>
+  //     <div className="flexImages">
+  //       <div className="imageWrapper">
+  //         <img className='globe' src={globe} alt="" />
+  //       </div>
+  //       <div className="imageWrapper up">
+  //         <img className='rect1' src={images[0]} alt="" />
+  //         <img className='rect2' id='top-upper' src={images[1]} alt="" />
+  //       </div>
+  //     </div>
+  //     <div className="flexImages down">
+  //       <div className="imageWrapper">
+  //         <img className='rect1' src={images[2]} alt="" />
+  //         <img className='rect2' src={images[3]} alt="" />
+  //       </div>
+  //     </div>
+  //     { showBottom ? <div className="flexImages" >
+  //       <div className="second"></div>
+  //       <div className="imageWrapper up1">
+  //         <img className='rect1' src={images[4]} alt="" />
+  //         <img className='rect2' id='top-upper' src={images[5]} alt="" />
+  //       </div>
+  //     </div> : <div></div>} 
+  //     { showBottom ? <div className="flexImages down1" >
+  //       <div className="imageWrapper ">
+  //         <img className='rect1 rectD1' src={images[6]} alt="" />
+  //         <img className='rect2 rectD2' src={images[7]} alt="" />
+  //       </div>
+  //       <div className="imageWrapper" >
+  //         <img className='mic' src={mic} alt="" />
+  //       </div>
+  //     </div> : <div></div> }  
+  //   </div>
+  function Speakers(props) {
+    function changeBackground(e) {
+      e.target.style.background = 'red';
+    }
+    return (
+      <div className="Speakers">
+      <h1>Speakers</h1>
+     <div className="flex">
+        <div className="flexs">
+          <img className="pic" src={image} alt="img" />
+          <div className="flex">
+            <div>
+              <b>{props.pos1}</b>
+            </div>
+            <div>{props.name1}</div>
+          </div>
         </div>
-        <div className="imageWrapper up">
-          <img className='rect1' src={images[0]} alt="" />
-          <img className='rect2' id='top-upper' src={images[1]} alt="" />
+  
+        <div className="flexs">
+          <img className="pic" src={image} alt="img" onMouseOver={changeBackground} />
+          <div className="flexs">
+            <div>
+              <b>{props.pos2}</b>
+            </div>
+            <div>{props.name2}</div>
+          </div>
         </div>
       </div>
-      <div className="flexImages down">
-        <div className="imageWrapper">
-          <img className='rect1' src={images[2]} alt="" />
-          <img className='rect2' src={images[3]} alt="" />
+    
+
+        <div className="flex">
+        <div className="flexs">
+          <img className="pic" src={image} alt="img" />
+          <div className="flex">
+            <div>
+              <b>{props.pos1}</b>
+            </div>
+            <div>{props.name1}</div>
+          </div>
+        </div>
+  
+        <div className="flexs">
+          <img className="pic" src={image} alt="img" />
+          <div className="flexs">
+            <div>
+              <b>{props.pos2}</b>
+            </div>
+            <div>{props.name2}</div>
+          </div>
         </div>
       </div>
-      { showBottom ? <div className="flexImages" >
-        <div className="second"></div>
-        <div className="imageWrapper up1">
-          <img className='rect1' src={images[4]} alt="" />
-          <img className='rect2' id='top-upper' src={images[5]} alt="" />
-        </div>
-      </div> : <div></div>} 
-      { showBottom ? <div className="flexImages down1" >
-        <div className="imageWrapper ">
-          <img className='rect1 rectD1' src={images[6]} alt="" />
-          <img className='rect2 rectD2' src={images[7]} alt="" />
-        </div>
-        <div className="imageWrapper" >
-          <img className='mic' src={mic} alt="" />
-        </div>
-      </div> : <div></div> }  
-    </div>
-  )
+      </div>
+    
+    );
+  
 }
 
-export default Speaker;
+export default Speakers;
